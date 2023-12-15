ac5542f314bd2c0a9c012a7c171b04d0ab816d01
Ethan Marchand <ethanjmarchand@gmail.com>
2023-12-15 16:14:18 -0700

    embedded our templates

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"path/filepath"
 
 	"github.com/ethanjmachand/lenslocked/controllers"
+	"github.com/ethanjmachand/lenslocked/templates"
 	"github.com/ethanjmachand/lenslocked/views"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
@@ -17,16 +17,16 @@ func main() {
 	r.Use(middleware.Logger)
 
 	r.Get("/", controllers.StaticHandler(
-		views.Must(views.Parse(filepath.Join("templates", "home.gohtml")))))
+		views.Must(views.ParseFS(templates.FS, "home.gohtml"))))
 
 	r.Get("/contact", controllers.StaticHandler(
-		views.Must(views.Parse(filepath.Join("templates", "contact.gohtml")))))
+		views.Must(views.ParseFS(templates.FS, "contact.gohtml"))))
 
 	r.Get("/faq", controllers.StaticHandler(
-		views.Must(views.Parse(filepath.Join("templates", "faq.gohtml")))))
+		views.Must(views.ParseFS(templates.FS, "faq.gohtml"))))
 
 	r.Get("/login", controllers.StaticHandler(
-		views.Must(views.Parse(filepath.Join("templates", "login.gohtml")))))
+		views.Must(views.ParseFS(templates.FS, "login.gohtml"))))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "404 | Page not found", http.StatusNotFound)
